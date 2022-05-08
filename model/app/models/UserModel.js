d114eac76683f31c5d56eb5a132a931b4879c0f5
afu <yulifu@douyu.tv>
2022-05-08 20:49:48 +0800

    change js

--- model/app/models/UserModel.js
+++ model/app/models/UserModel.js
@@ -17,17 +17,17 @@ module.exports = class UserModel extends Model {
     rules() {
         return [
             {
-                rule: 'candy/model/RequiredValidator',
+                rule: 'candy/model/validators/RequiredValidator',
                 attributes: ['user_name', 'password', 'email'],
                 messages: ['用户名不能为空', '密码不能为空', '邮箱不能为空']
             },
             {
-                rule: 'candy/model/EqualValidator',
+                rule: 'candy/model/validators/EqualValidator',
                 attributes: ['password', 'confirming'],
                 messages: ['两次密码不一致']
             },
             {
-                rule: 'candy/model/EmailValidator',
+                rule: 'candy/model/validators/EmailValidator',
                 attributes: ['email'],
                 messages: ['邮箱格式不正确']
             }
