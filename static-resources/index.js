e22f512b49695056fa369da6d1b4cd497735191c
afu <yulifu@douyu.tv>
2022-05-13 10:56:34 +0800

    change js

--- static-resources/index.js
+++ static-resources/index.js
@@ -4,7 +4,7 @@ const Hook = require('candyjs/core/Hook');
 const R = require('candyjs/midwares/Resource');
 
 // 配置静态资源目录
-Hook.addHook(new R(__dirname + '/public').serve());
+Hook.addHook(R.serve(__dirname + '/public'));
 
 new CandyJs(new App({
     'id': 1,
