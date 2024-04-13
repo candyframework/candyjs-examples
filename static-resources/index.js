d7b8ce653dfa3c72d6d2c6200dd690a6a736d23a
afu <yulifu@douyu.tv>
2024-04-13 13:24:00 +0800

    change demo

--- static-resources/index.js
+++ static-resources/index.js
@@ -4,7 +4,10 @@ const Hook = require('candyjs/core/Hook');
 const R = require('candyjs/midwares/Resource');
 
 // 配置静态资源目录
-Hook.addHook(R.serve(__dirname + '/public'));
+// before 4.19
+// Hook.addHook(R().serve(__dirname + '/public'));
+// since 4.20
+Hook.addHook(new R(__dirname + '/public').serve());
 
 new CandyJs(new App({
     'id': 1,
